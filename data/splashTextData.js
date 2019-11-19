583ba57b72fd3745b236587d8a08fc75ba411a98
CodeSpider’n <57020076+SpiderGamin@users.noreply.github.com>
2019-11-18 19:04:39 -0800

    Update splashTextData.js

--- data/splashTextData.js
+++ data/splashTextData.js
@@ -2,6 +2,14 @@ var splashTextArray = ["Venturing Into The Unknown...",
 "Installing Virus...because you dont have virus protection",
 "Deleting Windows...wait, nevermind",
 "Nope... testa",
+"oooooo a game",
+"Stop complainin",
+"Apple ",
+"sqrt (-1) love you, some dont get it",
+"Villanger news",		       
+"Rage Quit",		       
+"Well, Stalked yet again",		       
+"Spam!!!!!!!!!!!!!! We love it!!",
 ];
 var splashText = splashTextArray[Math.floor(Math.random()*splashTextArray.length)];
 
@@ -12,10 +20,13 @@ if(currentDate.indexOf("Jan 01") !== -1){
 if(currentDate.indexOf("Feb 14") !== -1){
 	splashText = "Happy Valentines!";
 }
-if(currentDate.indexOf("Sep 12") !== -1){
-	splashText = "6 Month Anniversary! V0.5 Released!";
+if(currentDate.indexOf("Nov 18") !== -1){
+	splashText = "Birth of this game, I say!";
 }
 if(currentDate.indexOf("Dec 25") !== -1){
 	splashText = "Merry Christmas!";
+}
+if(currentDate.indexOf("Jan 21") !== -1){
+	splashText = "Happy Birthday SpiderGamin!";
 }
 document.getElementById("splashText").textContent = splashText;
