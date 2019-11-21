7b5fb3f57d4e9c8f454fdb52aaac5533fc182430
SpiderGamin <jaytaygame@gmail.com>
2019-11-21 12:08:36 -0800

    Update variable.js

--- variable.js
+++ variable.js
@@ -1,6 +1,6 @@
 // Variables in save function
 
-var versionNumber = "V0.0.0.1 Alpha"; var companyName = "To Be Named";
+var versionNumber = "V 0.0.2 Dev"; var companyName = "To Be Named";
 
 // unlocked & activated
 
